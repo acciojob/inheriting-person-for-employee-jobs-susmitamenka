46ad5ca16b6086a7326f04785a3a3ef255a99007
susmitamenka <susmitaprasad73@gmail.com>
2025-03-03 16:42:02 +0530

    created a submission

--- script.js
+++ script.js
@@ -4,41 +4,29 @@
 // function Employee(name, age, jobTitle) {}
 
 // // Do not change code below this line
-
-
 class Person {
-  constructor(name, age) {
-    this.name = name;
-    this.age = age;
-  }
+    constructor(name, age) {
+        this.name = name;
+        this.age = age;
+    }
 
-  greet() {
-    console.log(`Hello, my name is ${this.name}, I am ${this.age} years old.`);
-  }
+    greet() {
+        console.log(Hello, my name is ${this.name} and I am ${this.age} years old.);
+    }
 }
 
-// Employee Class (Inheriting from Person)
 class Employee extends Person {
-  constructor(name, age, jobTitle) {
-    super(name, age); // Call the constructor of the Person class
-    this.jobTitle = jobTitle;
-  }
+    constructor(name, age, jobTitle) {
+        super(name, age);
+        this.jobTitle = jobTitle;
+    }
 
-  jobGreet() {
-    console.log(`Hello, my name is ${this.name}, I am ${this.age} years old, and my job title is ${this.jobTitle}.`);
-  }
+    jobGreet() {
+        console.log(Hello, my name is ${this.name}, I am ${this.age} years old, and my job title is ${this.jobTitle}.);
+    }
 }
 
-
-const person = new Person("Alice", 25);
-person.greet();
-
-
-const employee = new Employee("Bob", 30, "Manager");
-employee.jobGreet();
-
+// Attach to window so Cypress can access them
 window.Person = Person;
 window.Employee = Employee;
-
-
 
